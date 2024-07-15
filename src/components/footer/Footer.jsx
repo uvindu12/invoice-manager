9ac7b6bd79703e442791c0278885970a184488d4
ZinoTrust <zinotrust@gmail.com>
2024-07-15 08:24:15 +0100

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,13 @@
 export default function Footer() {
   return (
     <>
-      <footer className="bg-color-dark-blue h-14 --center-all">
+      <div className="bg-color-dark-blue h-14 --center-all">
         <div className="container flex-center">
           <p className="text-sm text-white">
             © ZinoTrust Academy - All Rights Reserved
           </p>
         </div>
-      </footer>
+      </div>
     </>
   );
 }
