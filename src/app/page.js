51d66abee01a519e483bd006776e0a3a90845129
uvindu <uvindukithmina47@gmail.com>
2025-03-04 17:26:58 +0530

    install alerDialog ui component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
+import CreateInvoice from "@/components/invoice/CreateInvoice";
+import ListInvoice from "@/components/invoice/ListInvoice";
+import { Separator } from "@/components/ui/separator";
+
 export default function Home() {
   return (
-    <div className="flrx justify-center min-h-[82vh]">
+    <div className="flex justify-center min-h-[82vh]">
       <section className= "w-full px-2 max-w-[1000px]">
-        <div className="flex judtify-between">
+        <div className="flex justify-between items-center">
           <h3 className= "text-2xl font-semibold">Invoice Manager</h3>
+          <CreateInvoice/>
         </div>
+        <Separator className ="my-2 border-b-[2px] border-color-light-blue"/>
+        <ListInvoice/>
       </section>
       
     </div>
