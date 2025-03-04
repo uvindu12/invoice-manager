029141ecb4776320f1b3fc229b611dd7bbfb2ee3
uvindu <uvindukithmina47@gmail.com>
2025-03-04 12:55:53 +0530

    First commmit

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <div className="flex-center min-h-[82vh]">
-      <h2>ZinoTrustAcademy.com</h2>
+    <div className="flrx justify-center min-h-[82vh]">
+      <section className= "w-full px-2 max-w-[1000px]">
+        <div className="flex judtify-between">
+          <h3 className= "text-2xl font-semibold">Invoice Manager</h3>
+        </div>
+      </section>
+      
     </div>
   );
 }
